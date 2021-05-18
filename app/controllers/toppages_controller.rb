e4f5b67b4a4cbe19ab07eecc0243a71cb6cb3b74
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-18 15:49:44 +0000

    rot

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,7 +1,7 @@
 class ToppagesController < ApplicationController
   def index
     @questions = Question.all
-    @rank_questions = Question.order(impressions_count: 'DESC')
+    @rank_questions = Question.order(impressions_count: 'desc')
     
   end
 end
