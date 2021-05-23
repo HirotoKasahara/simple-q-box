491e6f62f0b746fefda60de564c8722efcbacc03
taro.kirameki <taro.kirameki@techacademy.jp>
2021-05-23 09:28:03 +0000

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @questions = @user.questions
     @sleep = @user.cakes.build
-   @m = current_user.mokuhyous.last
+   
  
    @todays_sleep = @user.cakes.last(2)[0]
   
